0e48fb2144bdfffb74765516fb2222e3b2be3c0d
Sushanta Das <sushanta.das.ju@gmail.com>
2024-07-12 14:56:51 +0530

    use tisutisu e2e

--- hack/sandbox-development-mode.sh
+++ hack/sandbox-development-mode.sh
@@ -9,5 +9,5 @@ $ROOT/hack/reduce-gitops-cpu-requests.sh
 echo
 echo "Installing the Toolchain (Sandbox) operators in dev environment:"
 rm -rf ${TOOLCHAIN_E2E_TEMP_DIR} 2>/dev/null || true
-git clone --depth=1 https://github.com/codeready-toolchain/toolchain-e2e.git ${TOOLCHAIN_E2E_TEMP_DIR}
+git clone --depth=1 --branch=appstudio https://github.com/tisutisu/toolchain-e2e.git ${TOOLCHAIN_E2E_TEMP_DIR}
 make -C ${TOOLCHAIN_E2E_TEMP_DIR} appstudio-dev-deploy-latest SHOW_CLEAN_COMMAND="make -C ${TOOLCHAIN_E2E_TEMP_DIR} appstudio-cleanup"
