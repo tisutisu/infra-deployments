9dac034814022499c1cb3331d265686a6b442cd1
Mykola Morhun <mmorhun@redhat.com>
2025-10-28 13:51:47 +0200

    Changes according to review
    Signed-off-by: Mykola Morhun <mmorhun@redhat.com>

--- hack/build/setup-namespace.sh
+++ hack/build/setup-namespace.sh
@@ -4,4 +4,3 @@
 
 CURRENT_NAMESPACE=$(oc config view --minify -o 'jsonpath={..namespace}')
 oc label namespace $CURRENT_NAMESPACE --overwrite argocd.argoproj.io/managed-by=gitops-service-argocd
-oc apply -f https://raw.githubusercontent.com/codeready-toolchain/member-operator/master/config/appstudio-pipelines-runner/base/appstudio_pipelines_runner_role.yaml
